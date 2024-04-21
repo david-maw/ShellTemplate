b0fa7d89f88ee0d5ebe3c9c2ca80653e3b2844d0
David Maw <david@maw.name>
2024-04-20 21:10:18 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShellTemplate
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
